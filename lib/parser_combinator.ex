a4adebc426e0e7468c6e59b37a77c88b32d9fcb2
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-11-15 10:50:23 +0200

    Basic combinators. Parse select statement. Add tests

--- lib/parser_combinator.ex
+++ lib/parser_combinator.ex
@@ -1,12 +1,33 @@
 defmodule ParserCombinator do
+  def input do
+    " select  column
+      from table"
+  end
+
   def run do
     input = input()
 
-    parser = identifier()
+    parser = select_statement()
     parser.(input)
   end
 
-  defp char() do
+  defp select_statement() do
+    sequence([
+      keyword(:select),
+      separated_list(token(), char(?,)),
+      keyword(:from),
+      token()
+    ])
+    |> map(fn [_select_kw, columns, _from_kw, from] ->
+      %{
+        statement: :select,
+        columns: columns,
+        from: from
+      }
+    end)
+  end
+
+  def char() do
     fn input ->
       case input do
         "" -> {:error, "cannot parse a char"}
@@ -15,7 +36,41 @@ defmodule ParserCombinator do
     end
   end
 
-  defp many(parser) do
+  def separated_list(element_parser, separator_parser) do
+    sequence([
+      element_parser,
+      sequence([separator_parser, element_parser]) |> many()
+    ])
+    |> map(fn [term, list] ->
+      [term | Enum.map(list, fn [_, t] -> t end)]
+    end)
+  end
+
+  def keyword(word) do
+    token() |> satisfy(fn term -> String.downcase(term) == String.downcase(to_string(word)) end)
+  end
+
+  def token() do
+    sequence([many(empty()), identifier(), many(empty())])
+    |> map(fn [_ls, term, _ts] -> term end)
+  end
+
+  def empty() do
+    choice([char(?\s), char(?\t), char(?\n), char(?\r)])
+  end
+
+  def char(a..b), do: char() |> satisfy(fn term -> term in a..b end)
+  def char(char), do: char() |> satisfy(fn term -> term == char end)
+  def digit(), do: char() |> satisfy(fn term -> term in ?0..?9 end)
+  def letter(), do: char() |> satisfy(fn term -> term in ?A..?Z or term in ?a..?z end)
+
+  def map(parser, function) do
+    fn input ->
+      with {:ok, term, rest} <- parser.(input), do: {:ok, function.(term), rest}
+    end
+  end
+
+  def many(parser) do
     fn input ->
       case parser.(input) do
         {:error, _} ->
@@ -28,23 +83,47 @@ defmodule ParserCombinator do
     end
   end
 
-  defp digit() do
-    char()
-    |> satisfy(fn term -> term in ?0..?9 end)
+  def sequence(parsers) do
+    fn input ->
+      case parsers do
+        [] ->
+          {:ok, [], input}
+
+        [parser | rest_parsers] ->
+          with {:ok, term, rest} <- parser.(input),
+               {:ok, other_terms, rest} <- sequence(rest_parsers).(rest),
+               do: {:ok, [term | other_terms], rest}
+      end
+    end
+  end
+
+  def choice(parsers) do
+    fn input ->
+      case parsers do
+        [] ->
+          {:error, "None of the parsers suceeded"}
+
+        [parser | rest_parsers] ->
+          case parser.(input) do
+            {:ok, term, rest} -> {:ok, term, rest}
+            {:error, _} -> choice(rest_parsers).(input)
+          end
+      end
+    end
   end
 
   def identifier() do
-    many(identifier_char())
+    non_digit = choice([letter(), char(?_)])
+
+    sequence([non_digit, many(identifier_char())])
+    |> map(fn term -> term |> to_string end)
   end
 
-  defp identifier_char() do
-    char()
-    |> satisfy(fn term ->
-      term in ?A..?Z or term in ?a..?z or term == ?_
-    end)
+  def identifier_char() do
+    choice([digit(), char(?_), char(?a..?z), char(?A..?Z)])
   end
 
-  defp satisfy(parser, acceptor) do
+  def satisfy(parser, acceptor) do
     fn input ->
       with {:ok, term, rest} <- parser.(input) do
         if(acceptor.(term)) do
@@ -54,9 +133,5 @@ defmodule ParserCombinator do
         end
       end
     end
-  end
-
-  defp input do
-    "select column from table"
   end
 end
--- test/parser_combinator_test.exs
+++ test/parser_combinator_test.exs
@@ -2,7 +2,85 @@ defmodule ParserCombinatorTest do
   use ExUnit.Case
   doctest ParserCombinator
 
-  test "greets the world" do
-    assert ParserCombinator.hello() == :world
+  describe "identifier" do
+    test "only chars" do
+      parser = ParserCombinator.identifier()
+      assert {:ok, "onlychars", ""} == parser.("onlychars")
+    end
+
+    test "with chars and underscore" do
+      parser = ParserCombinator.identifier()
+      assert {:ok, "value_with_underscore", ""} == parser.("value_with_underscore")
+    end
+
+    test "with chars and digits" do
+      parser = ParserCombinator.identifier()
+      assert {:ok, "value1", ""} == parser.("value1")
+    end
+
+    test "with chars and digits and underscore" do
+      parser = ParserCombinator.identifier()
+      assert {:ok, "value_1", ""} == parser.("value_1")
+    end
+
+    test "can start with an underscore" do
+      parser = ParserCombinator.identifier()
+      assert {:ok, "_value_1", ""} == parser.("_value_1")
+    end
+
+    test "cannot start with a digit" do
+      parser = ParserCombinator.identifier()
+      assert match?({:error, _}, parser.("1_value_1"))
+    end
+  end
+
+  describe "token" do
+    test "single value" do
+      parser = ParserCombinator.token()
+      assert {:ok, "value", ""} == parser.("value")
+    end
+
+    test "multiple values" do
+      parser = ParserCombinator.token()
+      {:ok, term, _} = parser.("  \t\n value value2")
+      assert term == "value"
+    end
+  end
+
+  describe "keyword" do
+    test "single value" do
+      parser = ParserCombinator.keyword(:value)
+      assert {:ok, "value", ""} == parser.("value")
+    end
+
+    test "multiple values" do
+      parser = ParserCombinator.keyword(:value)
+      {:ok, term, _} = parser.("  \t\n value value2")
+      assert term == "value"
+    end
+  end
+
+  describe "separated list" do
+    test "single value" do
+      parser =
+        ParserCombinator.separated_list(ParserCombinator.token(), ParserCombinator.char(?,))
+
+      assert {:ok, ["value"], ""} == parser.("value")
+    end
+
+    test "multiple values no whitespace" do
+      parser =
+        ParserCombinator.separated_list(ParserCombinator.token(), ParserCombinator.char(?,))
+
+      assert {:ok, ["value1", "value2", "value3"], ""} == parser.("value1,value2,value3")
+    end
+
+    test "multiple values with whitespace" do
+      parser =
+        ParserCombinator.separated_list(ParserCombinator.token(), ParserCombinator.char(?,))
+
+      assert {:ok, ["value1", "value2", "value3"], ""} ==
+               parser.(" value1  ,\n\n value2,\t  value3 ")
+    end
   end
 end
